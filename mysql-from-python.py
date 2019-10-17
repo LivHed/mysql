8a3d914947a50a730d4e92674e7dec85d133e0db
Ubuntu <ubuntu@ip-172-31-92-113.ec2.internal>
2019-10-17 14:23:14 +0000

    Delete names from the list with sql

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -12,12 +12,12 @@ connection = pymysql.connect(host='localhost',
                              db='Chinook')
                         
 try:
-    # Run a query
-    with connection.cursor(pymysql.cursors.DictCursor) as cursor:
-        sql = "SELECT * FROM Genre;"
-        cursor.execute(sql)
-        for row in cursor:
-           print(row)
+    with connection.cursor() as cursor:
+        list_of_names = ['fred']
+         # Prepare a string with same number of placeholders as in list_of_names
+        format_strings = ','.join(['%s'] * len(list_of_names))
+        cursor.execute("DELETE FROM Friends WHERE name in ({});".format(format_strings),
+            list_of_names)
+        connection.commit()
 finally:
-    # Close the connection, regardless of whether or not the query above was successful.
     connection.close()
